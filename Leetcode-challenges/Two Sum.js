3511260f67a6e59e11259234951c7434f38b1c94
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-01-19 21:20:21 +0500

    Starting the repository

---
+++ Leetcode-challenges/Two Sum.js
@@ -0,0 +1,14 @@
+const twoSum = function(nums, target) {
+    // A hash table provides much faster look up times
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const element = target - nums[i];
+        // Populating and traversing the given array only once
+        if (map.has(element)){
+            return [map.get(element), i]
+        }
+        map.set(nums[i], i);
+    }
+};
+
+console.log(twoSum([3, 5, 84, 456, 3], 6));
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # coding-problems
+A collection of solutions to various coding and programming challenges found on coding websites.
