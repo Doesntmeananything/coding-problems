32d821dc92d6be604adf38ccb3dc026e2e7d1c5e
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-03-19 22:20:47 +0300

    Weekly update

--- Algorithms/Remove Element.js
+++ Algorithms/Remove Element.js
@@ -9,7 +9,17 @@ The order of elements can be changed. It doesn't matter what you leave beyond th
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {};
+var removeElement = function(nums, val) {
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] == val) {
+      nums.splice(i, 1);
+    } else {
+      i += 1;
+    }
+  }
+  return nums.length;
+};
 
 console.log(removeElement([3, 2, 2, 3], 3));
 console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
