ae655f984338e9ada4ab16fc5ef297e78b4f49fa
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-04-06 22:33:28 +0300

    Weekly update

--- Algorithms/Add Binary.js
+++ Algorithms/Add Binary.js
@@ -4,19 +4,37 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-  const stringToLoop = a.length > b.length ? a : b;
+  if (a == b && a == "0") return "0";
   let result = "";
   let carry = 0;
-  for (let i = stringToLoop.length - 1; i >= 0; i--) {
-    if (a[i] == 1 && b[i] == 1) {
+  const longerString = a.length > b.length ? a : b;
+  const shorterString = a.length > b.length ? b : a;
+  for (let i = longerString.length - 1; i >= 0; i--) {
+    let parallelNumber =
+      Number(shorterString[i - (longerString.length - shorterString.length)]) ||
+      0;
+    let sum = Number(longerString[i]) + parallelNumber + carry;
+    if (sum > 1) {
       carry = 1;
+      result = sum - 2 + result;
+    } else if (sum == 0 && carry == 0) {
       result = "0" + result;
-    } else if (carry == 1 && (a[i] == 0 || b[i] == 0)) {
+    } else {
       carry = 0;
       result = "1" + result;
     }
   }
+  if (carry == 1) {
+    result = "1" + result;
+  }
+
+  return result;
 };
 
-console.log(addBinary("11", "1"));
-console.log(addBinary("1010", "1011"));
+console.log(addBinary("100", "110010"));
+// console.log(addBinary("11", "1"));
+// console.log(addBinary("0", "0"));
+// console.log(addBinary("1", "1"));
+// console.log(addBinary("1", "0"));
+// console.log(addBinary("1010", "1011"));
+// console.log(addBinary("1110111110", "1010110011"));
