024096fa5b1f99cd318898b079647a9d9272845a
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-04-13 20:34:29 +0300

    Weekly update

---
+++ Algorithms/Sqrt(x).js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+  return Math.floor(Math.sqrt(x));
+};
+
+console.log(mySqrt(4));
+console.log(mySqrt(8));
+console.log(mySqrt(0));
+console.log(mySqrt(1));
