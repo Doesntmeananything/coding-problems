948df3c7bc325dec303d805fc736d420e8b84d3b
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-04-24 20:27:56 +0300

    Weekly update

--- Algorithms/Merge Sorted Arrays.js
+++ Algorithms/Merge Sorted Arrays.js
@@ -31,9 +31,3 @@ var merge = function(nums1, m, nums2, n) {
 console.log(merge([1, 2, 3], 3, [2, 5, 6], 3));
 console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
 console.log(merge([0], 3, [0], 3));
-
-// Input:
-// nums1 = [1,2,3,0,0,0], m = 3
-// nums2 = [2,5,6],       n = 3
-
-// Output: [1,2,2,3,5,6]
