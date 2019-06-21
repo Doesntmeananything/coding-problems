e56d0a59a8a7fec238a3123220e703f3dddd1757
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-06-21 18:31:00 +0300

    Update challenges

---
+++ Algorithms/Remove the minimum.js
@@ -0,0 +1,13 @@
+function removeSmallest(numbers) {
+  let result = [...numbers];
+  result.splice(result.indexOf(Math.min(...result)), 1);
+  return result;
+}
+
+console.log(
+  removeSmallest([1, 2, 3, 4, 5]),
+  removeSmallest([5, 3, 2, 1, 4]),
+  removeSmallest([2, 2, 1, 2, 1]),
+  removeSmallest([1]),
+  removeSmallest([])
+);
