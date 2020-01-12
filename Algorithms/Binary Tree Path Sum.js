900d14f40bea412b3b45cfd06e2a856f17a6e916
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-01-12 15:11:58 +0300

    Update challenges

---
+++ Algorithms/Binary Tree Path Sum.js
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/path-sum/
+ *
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+const hasPathSum = (root, sum) => {
+  if (!root) return false;
+
+  if (!root.left && !root.right) return sum == root.val;
+
+  return (
+    hasPathSum(root.left, sum - root.val) ||
+    hasPathSum(root.right, sum - root.val)
+  );
+};
