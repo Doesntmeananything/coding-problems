2932dbe1043855acc85ef5a68940973c17112031
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-01-12 16:14:41 +0300

    Update challenges

--- Algorithms/Pascal's Triangle.js
+++ Algorithms/Pascal's Triangle.js
@@ -6,11 +6,9 @@
  */
 const generate = numRows => {
   let result = [];
-  for (let k = 1; k <= numRows; k++) {
-    result.push([...Array(k)].fill(1));
-  }
 
   for (let i = 0; i < numRows; i++) {
+    result.push(Array(i + 1).fill(1));
     for (let j = 1; j < i; j++) {
       result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
     }
