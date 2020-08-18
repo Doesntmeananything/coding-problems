8f779c44ed4907dd6364799bf1b59a6913181a46
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2020-08-19 00:30:18 +0300

    Add initial DP analysis program

---
+++ Dynamic Programming/fibonacci.js
@@ -0,0 +1,17 @@
+// Since computing n fibonacci number requires only two previous results
+// we save them for the duration of one loop
+const fib = (n) => {
+  if (n == 1 || n == 2) return 1;
+
+  let prev = 1;
+  let curr = 1;
+
+  for (let i = 3; i <= n; i++) {
+    let sum = prev + curr;
+    prev = curr;
+    curr = sum;
+  }
+  return curr;
+};
+
+console.log(fib(20));
